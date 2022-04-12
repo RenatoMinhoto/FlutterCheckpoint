df5a4c3c6a297811a3e2127aa799a88f5867a3ff
RenatoMinhoto <minhotorenato@gmail.com>
2022-04-12 00:15:41 -0300

    Logo update

--- lib/src/app/utils/ui_text.dart
+++ lib/src/app/utils/ui_text.dart
@@ -5,4 +5,4 @@ const String question = 'Pros of Flutter app development?';
 const String answer =
     ' 1. One codebase for all platforms \n 2. “It’s all Widgets” principle offers countless possibilities \n 3. Rich libraries \n 4. Fast testing with hot reload ' ;
 const String curiosity2 = ' 1. Giraffes dont have vocal cords \n 2. Butterflies have approximately 12 million eyes. \n 3. All mammals are capable of jumping except elephants \n 4. A flea can jump up to 350 times its height';
-const String curiosity1 = 'Curiosities about animals!';
+const String curiosity1 = 'Curiosities about animals! ';
